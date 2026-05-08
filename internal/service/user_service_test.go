3cd68dfeba189a7e1a5acd7fa945471ce809f689
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-05-07 22:20:29 -0600

    Added unit test for the users service.

--- internal/service/user_service_test.go
+++ internal/service/user_service_test.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/Mr-Rafael/finance-calculator/internal/db"
+)
+
+func TestRegisterUserAlreadyExists(t *testing.T) {
+	mockUsersRepo := &MockUsersRepo{
+		CreateUserFunc: func(ctx context.Context, params db.CreateUserParams) (db.User, error) {
+			return db.User{}, fmt.Errorf("email field violates unique constraint")
+		},
+	}
+	service := NewUserService(mockUsersRepo)
+	ctx := context.Background()
+
+	input := RegisterUserInput{
+		Email:    "existing@user.com",
+		Password: "password",
+		Username: "username",
+	}
+
+	_, err := service.RegisterUser(ctx, input)
+	if err == nil {
+		log.Fatalf("Expected an error to return for existing user, but none did.")
+	}
+}
